a68210002f91242a579ebda69ebe58041b29fcff
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2016-12-20 23:46:35 +0100

    Chapter 1 Commit 3

--- TDD1/src/test/java/com/pskwiercz/DollarTest.java
+++ TDD1/src/test/java/com/pskwiercz/DollarTest.java
@@ -17,8 +17,10 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.getAmount());
+        Dollar product = five.times(2);
+        assertEquals(10, product.getAmount());
+        product = five.times(3);
+        assertEquals(15, product.getAmount());
     }
 
 }
